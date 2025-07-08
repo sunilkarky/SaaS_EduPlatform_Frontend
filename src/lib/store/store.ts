12bb5b2dd0f0f108ef68c731696f73d1f48cc393
sunilkarky <sunilkarki670@gmail.com>
2025-07-08 12:25:35 +0545

    Custom useselector n useDispatch

--- src/lib/store/store.ts
+++ src/lib/store/store.ts
@@ -4,9 +4,12 @@ import teacherSlice from "./teacherSlice";
 
 const store=configureStore({
     reducer:{
-        userSlice:userSlice,
-        teacherSlice:teacherSlice
+        user:userSlice,
+        teacher:teacherSlice
     }
 })
 
 export default store
+//by default Store and slice access garne hooks ko type hudaina so use use type from store to assign type
+export type AppDispatch=typeof store.dispatch //useDispatch(CUD) lai type dina chayenxa in creating custom hooks.ts
+export type RootState=ReturnType<typeof store.getState>  //for useselector(select operation) custom hooks with type
